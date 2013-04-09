0b9cd264652eb7d581867aff8a9c122b932929f9
Anthony <anthony@MicroHulk-2.local>
2013-04-09 11:40:38 -0700

    Initial Commit

---
+++ NumberGueser/NumberGueser/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NumberGueser
+//
+//  Created by Anthony on 4/9/13.
+//  Copyright (c) 2013 Anthony. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
